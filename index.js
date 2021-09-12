531f49c4a1134be82fd7dee4adfa0efc1609aacd
Alexander <alexander-kilyushin@gmail.com>
2021-09-12 18:57:29 +0300

    add .prettierrc

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-const http = require('http');
-const path = require('path');
-const readline = require('readline');
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const http = require('http')
+const path = require('path')
+const readline = require('readline')
 
-const { getReqData } = require("./utils");
+const { getReqData } = require('./utils')
 
 const rl = readline.createInterface({
 	input: process.stdin,
-	output: process.stdout
-});
+	output: process.stdout,
+})
 
-rl.question("Enter project path:", async input => {
+rl.question('Enter project path:', async input => {
 	const pathArr = input.split('/')
 	const dirPathJoined = path.join('/', ...pathArr)
 
 	rl.close()
 
-	const HOSTNAME = '127.0.0.1';
-	const PORT = 7095;
+	const HOSTNAME = '127.0.0.1'
+	const PORT = 7095
 
 	const server = http.createServer(async (req, res) => {
 		// res.statusCode = 200
@@ -34,32 +34,34 @@ rl.question("Enter project path:", async input => {
 		console.log(reqData)
 
 		const execCommand = async (dirPathJoined, command) => {
-			const { error, stderr, stdout } = await exec(command, { cwd: dirPathJoined });
-		
+			const { error, stderr, stdout } = await exec(command, {
+				cwd: dirPathJoined,
+			})
+
 			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
+				console.log(`error: ${error.message}`)
+				return
 			}
-		
+
 			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
+				console.log(`stderr: ${stderr}`)
+				return
 			}
-		
-			console.log(`stdout: ${stdout}`);
+
+			console.log(`stdout: ${stdout}`)
 		}
-		
+
 		await execCommand(dirPathJoined, 'ls')
 		// await execCommand(dirPathJoined, 'git fetch origin')
 		// await execCommand(dirPathJoined, 'git pull origin master')
 		// await execCommand(dirPathJoined, 'docker-compose down')
 		// await execCommand(dirPathJoined, 'docker rmi portal_server portal_client')
 		// await execCommand(dirPathJoined, 'docker-compose up')
-	});
+	})
 
 	server.listen(PORT, HOSTNAME, () => {
-		console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-	});
+		console.log(`Server running at http://${HOSTNAME}:${PORT}/`)
+	})
 })
 
 // /home/me/Documents/code-other/test1
