76ddb439c714cd60117e53339574c0ebf3599466
Alexander <alexander-kilyushin@gmail.com>
2021-09-12 20:02:29 +0300

    uncommend deploy commands

--- index.js
+++ index.js
@@ -17,53 +17,38 @@ rl.question('Enter project path:', async input => {
 
 	rl.close()
 
-	const HOSTNAME = '127.0.0.1'
-	const PORT = 7095
-
 	const server = http.createServer(async (req, res) => {
-		// res.statusCode = 200
-		// res.setHeader('Content-Type', 'application/json')
-		// res.end('Hello World')
-
-		console.log('hit')
-
-		if (req.url !== '/update-portal-app') {
-			console.log('url is not [/update-portal-app], return')
-			return
-		}
-
-		// console.log(req)
+		if (req.url !== '/update-portal-app') return
 
 		const reqData = await getReqData(req)
-		console.log(JSON.parse(reqData).ref)
+		if (JSON.parse(reqData).ref !== 'refs/heads/master') return
 
-		// const execCommand = async (dirPathJoined, command) => {
-		// 	const { error, stderr, stdout } = await exec(command, {
-		// 		cwd: dirPathJoined,
-		// 	})
+		const execCommand = async command => {
+			const { error, stderr, stdout } = await exec(command, {
+				cwd: dirPathJoined,
+			})
 
-		// 	if (error) {
-		// 		console.log(`error: ${error.message}`)
-		// 		return
-		// 	}
+			if (error) {
+				console.log(`error: ${error.message}`)
+				return
+			}
 
-		// 	if (stderr) {
-		// 		console.log(`stderr: ${stderr}`)
-		// 		return
-		// 	}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`)
+				return
+			}
 
-		// 	console.log(`stdout: ${stdout}`)
-		// }
+			console.log(`stdout: ${stdout}`)
+		}
 
-		// await execCommand(dirPathJoined, 'ls')
-
-		// await execCommand(dirPathJoined, 'git fetch origin')
-		// await execCommand(dirPathJoined, 'git pull origin master')
-		// await execCommand(dirPathJoined, 'docker-compose down')
-		// await execCommand(dirPathJoined, 'docker rmi portal_server portal_client')
-		// await execCommand(dirPathJoined, 'docker-compose up')
+		await execCommand('git fetch origin')
+		await execCommand('git pull origin master')
+		await execCommand('docker-compose down')
+		await execCommand('docker rmi portal_server portal_client')
+		await execCommand('docker-compose up')
 	})
 
+	const PORT = 7095
 	server.listen(PORT, () => {
 		console.log(`Server running at http://localhost:${PORT}/`)
 	})
